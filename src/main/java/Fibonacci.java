0865f5d3dd9d6c3760298158293390b3902314e9
Paul Davis <pdavisDU@gmail.com>
2023-09-15 21:48:56 +0000

    finally passed testing

--- src/main/java/Fibonacci.java
+++ src/main/java/Fibonacci.java
@@ -1,20 +1,34 @@
-
 public class Fibonacci {
     /**
-     * Return the nth number of fibonacci sequence.
-     * The fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next
-     * number. The first two numbers in the whole fibonacci sequence are always 0 and 1.
+     * Return the nth number of the Fibonacci sequence.
+     * The Fibonacci sequence is calculated by adding the previous two numbers in the sequence to obtain the next number.
+     * The first two numbers in the whole Fibonacci sequence are always 0 and 1.
      * For instance, the sequence starts as:
      * 0 1 1 2 3 5 8 13 21
-     * where the 4th number of the sequence would be 3, the 5th number would be 5, the 6th number would be 8, etc.
+     * where the 1st number of the sequence would be 0, the 2nd number would be 1, the 3rd number would be 1, etc.
      *
-     * You could either use a for loop to keep track of the current number of the fibonacci sequence as well as the two
-     * numbers before it, or you could look up a recursive solution to experiment with recursion for the first time.
-     *
-     * @param n an iteration of the fibonacci sequence.
-     * @return the nth number of fibonacci sequence.
+     * @param n an iteration of the Fibonacci sequence (1-based).
+     * @return the nth number of the Fibonacci sequence.
      */
-    public int fib(int n){
-        return 0;
+    public int fib(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Input must be a positive integer");
+        }
+
+        if (n == 1 || n == 2) {
+            return 1; // The first two numbers in the Fibonacci sequence are both 1
+        } else {
+            int a = 1;
+            int b = 1;
+            int result = 0;
+
+            for (int i = 3; i <= n; i++) {
+                result = a + b;
+                a = b;
+                b = result;
+            }
+
+            return result;
+        }
     }
 }
